41a9be692121d2d687304e6e5bed8a755fe4f85a
im4nu <victorx811@gmail.com>
2023-12-12 07:43:47 -0300

    added a allert when opened on desktop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -40,7 +41,31 @@ export default function RootLayout({
         <meta name="theme-color" content="#ffffff" />
       </head>
 
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="xl:hidden">{children}</div>
+        <div className="hidden xl:flex items-center justify-center w-screen h-screen bg-black/40">
+          <div className="flex bg-white rounded-xl text-center items-center justify-center p-8 w-80 flex-col gap-8">
+            <h1 className="text-xl font-medium">Oopps...</h1>
+
+            <Image
+              alt="Ilustração de erro"
+              src={"/images/sad_ilustration.svg"}
+              width={278}
+              height={208}
+            />
+
+            <p className="text-sm">
+              O nosso projeto funciona apenas em dispositivos móveis,
+              <span className="font-bold text-gray-700">
+                experimente abir o projeto em um smartphone ou tablet
+              </span>
+            </p>
+            <button className="bg-main rounded-lg py-4 w-[200px] flex flex-row text-white items-center justify-center gap-2">
+              Tudo bem!
+            </button>
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
